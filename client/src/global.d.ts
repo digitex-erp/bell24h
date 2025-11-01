ea205438ae48df76315c0f68b43c09cbe367d5c2
Bell24h Developer <digitex.studio@gmail.com>
2025-11-01 23:22:40 +0530

    Fix: Add TypeScript declarations for window.ethereum

---
+++ client/src/global.d.ts
@@ -0,0 +1,9 @@
+﻿// TypeScript declarations for Web3/Ethereum extensions
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean;
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on?: (event: string, handler: (...args: any[]) => void) => void;
+    removeListener?: (event: string, handler: (...args: any[]) => void) => void;
+  };
+}
